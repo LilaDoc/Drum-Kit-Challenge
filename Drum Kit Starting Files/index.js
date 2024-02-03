8e3bc403550e769d48a7953926e85d7fcff2fa86
lila doc <lilajacquetlj@gmail.com>
2024-02-03 08:03:59 +0100

    First commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,80 @@
+
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+ });
+
+var buttonList= document.querySelectorAll(".drum");
+
+
+for (i=0; i<buttonList.length+1;i++){
+   
+    buttonList[i].addEventListener("click", function(){
+        var buttonInnerHTML = this.innerHTML;
+
+      makeSound(buttonInnerHTML);
+      buttonAnimation(buttonInnerHTML);
+    });
+}
+
+
+function makeSound(drumKey){
+  switch(drumKey){
+    case "w":
+        var crash= new Audio('./sounds/crash.mp3');
+        crash.play();  
+        break;
+       
+        case "a":
+        var kick= new Audio('./sounds/kick-bass.mp3');
+        kick.play();
+        break;
+        
+        case "s":
+        var snare= new Audio('./sounds/snare.mp3');
+        snare.play();
+        break;
+
+        case "d":
+        var tom1= new Audio('./sounds/tom-1.mp3');
+        tom1.play();         
+        break;
+
+        case "j":
+        var tom2= new Audio('./sounds/tom-2.mp3');
+        tom2.play();       
+        break;
+
+        case "k":
+         var tom3= new Audio('./sounds/tom-3.mp3');
+        tom3.play();
+                  
+        break;
+        case "l":
+        var tom4= new Audio('./sounds/tom-4.mp3');
+        tom4.play();       
+        break;
+
+  
+        default:console.log(drumkey);
+  }
+}
+
+
+function buttonAnimation(currentKey){
+
+  var selectedKey =document.querySelector("."+ currentKey);
+
+  selectedKey.classList.add("pressed");
+
+  setTimeout(function(){
+    selectedKey.classList.remove("pressed");
+  },100);
+
+  
+}
+// buttonList[0].addEventListener("click",function(){alert("Boom")});
+// document.querySelector("button").addEventListener("click",function(){alert("Boom")});
+
